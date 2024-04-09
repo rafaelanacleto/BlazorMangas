3803f936fc0e560b8904ad4bd780183362f31e2b
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-04-09 08:48:57 -0300

    Adcionado git ignore e mudanças no front end

--- BlazorMangas/obj/Debug/net8.0/BlazorMangas.AssemblyInfo.cs
+++ BlazorMangas/obj/Debug/net8.0/BlazorMangas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorMangas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae1e99ab848a2aebf3502fdaca8adb73eb9cb702")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4924e925b45d176f01aa1548eb4cf0012ac18b7d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorMangas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorMangas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
